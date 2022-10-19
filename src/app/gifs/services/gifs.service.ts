7766647ee958b4eabe837acff82a470dd9d9a8a4
Bryan Jimenez <bryanbg.jg@gmail.com>
2022-10-19 11:00:05 -0400

    add: gifs in LocalStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { SearchGifsResponse, Gif } from '../interface/gifs-interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  private apiKey : string = 'PEemMhoHxegoo8hXdb7WApTLAnsnDAy8';
   private _record: string[] = [];
+
+  public results: Gif[] = [];
 
   get record(){
     return [...this._record];
+  }
+
+  constructor(private http: HttpClient){
+    this._record = JSON.parse(localStorage.getItem('record')!) || [];
+    this.results = JSON.parse(localStorage.getItem('results')!) || [];
   }
 
   searchGifs(query: string){
@@ -18,8 +28,16 @@ export class GifsService {
     if(!this._record.includes(query)){
       this._record.unshift(query);
       this._record = this._record.slice(0, 10);
+
+      localStorage.setItem('record', JSON.stringify(this._record));
+      
     }
     
-    console.log(this._record);
-  }
+   this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=PEemMhoHxegoo8hXdb7WApTLAnsnDAy8&q=${query}&limit=10`)
+   .subscribe((resp)  => {
+    console.log(resp.data);
+    this.results = resp.data;
+    localStorage.setItem('results', JSON.stringify(this.results));
+   });
+ }
 }
